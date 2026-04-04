624be550e28c21c7c2c236b7b89781ff300c4303
DilshanGAM <malithdilshan27@gmail.com>
2026-04-04 20:58:42 +0530

    day 11 ✏️

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+  
+  
+  return (
+    <>
+      <ProductCard name="MacBook Air" price="LKR.250,000/-" photo="https://picsum.photos/id/1/200/300"/>
+
+      <ProductCard name="Apple iPhone 4s" price="LKR.50,000/-" photo="https://picsum.photos/id/3/200/300"/>
+
+      <ProductCard name="Smart signature pen" price="LKR.5,000/-" photo="https://picsum.photos/id/4/200/300"/>
+    </>
+  )
+}
+
+export default App
